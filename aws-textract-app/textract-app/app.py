680d88c0147df95fde66c820df92bc05e3bb9086
adiaga006 <85040268+adiaga006@users.noreply.github.com>
2022-12-07 17:35:52 +0700

    Update app.py

--- aws-textract-app/textract-app/app.py
+++ aws-textract-app/textract-app/app.py
@@ -1,8 +1,9 @@
 from flask import render_template, Flask, request
 import boto3
 import json
-account_id = "AKIAUI4FU6N5QJWMFBEB"
-account_key = "FtMSEtY261s/zhg3fScQ//AocSS3mNPDmeHT8wkS"
+account_id = "ASIAWIM27RKEGAXZ5S7V"
+account_key = "rG3chr52yy2pt9ymuDUyYp5o5ldFCGgNcNZVzbNh"
+aws_token="FwoGZXIvYXdzEBQaDC4ukJtxlnf4O36FNSLPAQy1Kpce+F+chc0fIVR6J12hGiSEbkUV2b3MioJnWmQ4Z+P3kznzTBgvOXleCDTwqzgA59afkMdxNyvtgAnpW42ghdJif5XQlNI6xnJrA/f5v90PU3IU8VAvrr2pwcHQbvXYzyDCHmrGPgyZo38DJ84YmTJxWSvjZZMPfCSHsSx5xqIH+tiojywV6/D9PDuhHECsGj7gFRfxV/WfX6QMdoeMBPyTh9HygQXja15zglfhkqC9bfEkWDHwyU/sBdOvLuzUh2lIT0+mpqw4YflfGCicz8GcBjItFhl4juM+j5FlCi2mjQKXSWE3a15+kS8ouLNs8Fy4370Ddos67+KIdRDXG28h"
 def client():
     global account_id
     global account_key
@@ -10,6 +11,7 @@ def client():
     print(account_id, account_key)
     client = boto3.client("textract", aws_access_key_id=account_id,
                           aws_secret_access_key=account_key,
+                          aws_session_token = aws_token,
                           region_name='us-east-1')
     return client
 app = Flask(__name__)
